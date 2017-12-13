d2e955bb21593d32edafb70848c50d1b3a818c29
qiyang <qi.yang@tendcloud.com>
2017-12-13 14:14:12 +0800

    feat():add card-contarier

--- src/views/base/chart.vue
+++ src/views/base/chart.vue
@@ -5,6 +5,53 @@
   <div class="hello">
     <div class="main-container">
       <i-row class="item-row" :gutter="24">
+        <i-col span="6" class="pr-8">
+          <cardcontaier
+            title="日均下单量"
+            content="32,625"
+            subContent="-12.15%"
+            status="error"
+            border
+            type="normal"
+          >
+          </cardcontaier>
+        </i-col>
+        <i-col span="6" class="pr-8">
+          <cardcontaier
+            title="日均下单量"
+            content="32,625"
+            subContent="-12.15%"
+            status="error"
+            :help="false"
+            border
+            type="normal"
+          >
+          </cardcontaier>
+        </i-col>
+        <i-col span="6" class="pr-8">
+          <cardcontaier
+            title="日均下单量"
+            content="32,625,318"
+            sub-content="-12.15%"
+            color="#19be6b"
+            border
+            type="center"
+          >
+            <!-- <span slot="header">日均下单量</span> -->
+          </cardcontaier>
+        </i-col>
+        <i-col span="6" class="pr-8">
+          <cardcontaier
+            content="32,625,318"
+            size="20"
+            sub-content="日均下单量"
+            color="#80848F"
+            border
+            type="left"
+          ></cardcontaier>
+        </i-col>
+      </i-row>
+      <i-row class="item-row" :gutter="24">
         <i-col span="12">
           <outercontaier class="mt-16"
                          title="30日趋势"
