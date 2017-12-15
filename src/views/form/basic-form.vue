ce4ef519f32e127aaa4c28eafe8f9abb3949cf82
wxnet2013 <wxnet2013@gmail.com>
2017-12-15 12:44:50 +0800

    chore: 调整css规范

--- src/views/form/basic-form.vue
+++ src/views/form/basic-form.vue
@@ -15,7 +15,7 @@
       <FormItem
         class="form-step-iform-item"
         v-for="(item, index) in formDynamic.items"
-        :label="labelName + (index + 1)">
+        :label="labelName + (index + 1)" :key="index">
         <Row>
           <iCol span="11">
             <iInput></iInput>
@@ -85,129 +85,5 @@
   };
 </script>
 <style lang="scss">
-.form-step {
-  padding: 48px;
-  background: #fff;
-  box-shadow: 0 1px 4px 0 #e3e8ee;
-  border-radius: 4px;
-
-  &-row {
-    padding-bottom: 30px;
-    border-bottom: 2px solid #ccc;
-
-    &-title {
-      font-family: PingFangSC-Semibold;
-      font-size: 12px;
-      color: #464c5b;
-      line-height: 32px;
-      font-weight: 600;
-      text-align: left;
-    }
-  }
-
-  &-close {
-    cursor: pointer;
-  }
-
-  &-info {
-    padding: 28px 0;
-    font-family: PingFangSC-Regular;
-    font-size: 12px;
-    color: #464c5b;
-    text-align: left;
-  }
-
-  &-iform {
-
-    &-item {
-      background: #f7f8f8;
-      padding: 8px 16px;
-
-      & > .ivu-form-item-label {
-        text-align: left !important;
-        font-family: PingFangSC-Semibold;
-        font-size: 14px;
-        font-weight: 600;
-        color: #464c5b;
-      }
-
-      & > .ivu-form-item-content {
-
-        & > .ivu-row {
-          & .ivu-form-item-label {
-            font-family: PingFangSC-Regular;
-            font-size: 12px;
-            color: #9ea7b4;
-          }
-
-          & .ivu-form-item-content {
-            width: 65%;
-            display: inline-block;
-          }
-        }
-      }
-    }
-
-    &-btn {
-      text-align: left;
-
-      & > .ivu-form-item-content {
-        margin-left: 0 !important;
-      }
-    }
-
-    &-set {
-      text-align: left;
-    }
-
-  }
-
-    @media (max-width: 1024px) {
-      &-iform {
-        padding-right: 0;
-        &-set {
-          padding-top: 30px;
-          margin-top: 30px;
-          position: relative;
-
-          &::before {
-            content: '';
-            width: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-            border-top: 2px solid #d7dde4;
-          }
-
-          & > .ivu-form-item-content {
-            margin-left: 0 !important;
-          }
-        }
-      }
-    }
-
-    @media (min-width: 1025px) {
-      &-iform {
-        padding-right: 20%;
-        &-set {
-          padding-top: 30px;
-          margin-top: 30px;
-          position: relative;
-
-          &::before {
-            content: '';
-            width: 125%;
-            position: absolute;
-            top: 0;
-            left: 0;
-            border-top: 2px solid #d7dde4;
-          }
-
-          & > .ivu-form-item-content {
-            margin-left: 0 !important;
-          }
-        }
-      }
-    }
-}
+@import "./basic-form.scss";
 </style>
